cb33c83de4ae4122ce235840d9232ee584457e56
NuttasitBoonwat <nuttasit_lom@hotmail.com>
2018-06-21 17:03:31 +0700

    Puen 2

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Sidebar from './Sidebar.js';
+import Header from './Header.js';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+         
+        </div>
+     
+
+
+    );
+  }
+}
+
+export default App;
