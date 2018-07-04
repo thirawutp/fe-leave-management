5400e0547889a75a88f1cab38656fe1ff06f5d64
Nuttasit Boonwat <nuttasit_lom@hotmail.com>
2018-07-04 16:01:06 +0700

    puen

--- src/components/Main/NavigationBar.js
+++ src/components/Main/NavigationBar.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
-import home from './home-3.png';
-import chart from './line-chart.png';
-import check from './check.png';
-import logout from './logout.png';
+import '../../App.css';
 import Header from './Header.js';
+import { Link } from 'react-router';
 
 class NavigationBar extends Component {
   render() {
     return (
       <div>
       	 <ul className='navigationbar-list'>
-          <li className='navigationbar-item'>
-            <a><img src={home} width="25" height="25" />     Home</a>
-          </li>
-          <li className='navigationbar-item'>
-            <a><img src={chart} width="25" height="25" />     Static</a>
-          </li>
-          <li className='navigationbar-item'>
-            <a><img src={check} width="25" height="25" />     Approve</a>
-          </li>
-          <li className='navigationbar-item'>
-            <a><img src={logout} width="25" height="25" />     Logout</a>
+            <li className='navigationbar-item'>
+                <Link to='/Leave'>leave</Link>
             </li>
-        </ul>
-        
+            <li className='navigationbar-item'>
+                <Link to='/History'>history</Link>
+            </li>
+            <li className='navigationbar-item'>
+                <Link to='/Approve'>Approve</Link>
+            </li>
+          </ul>
       </div>
     );
   }
 }
 
 export default NavigationBar;
+
