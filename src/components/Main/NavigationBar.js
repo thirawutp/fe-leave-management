6566919f645a593edac591ea7909087cf0929028
Sirapob Meechoovet <gunzo@Sirapobs-MBP.true.true>
2018-07-19 17:59:36 +0700

    asdfghj

--- src/components/Main/NavigationBar.js
+++ src/components/Main/NavigationBar.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import '../../App.css';
-import Header from './Header.js';
 import { Link } from 'react-router';
 import logout from '../../asset/images/logout.png';
 
 
 class NavigationBar extends Component {
     activeClassName(pathname) {
-        if(this.props.location.pathname === pathname) {
+        if (this.props.location.pathname === pathname) {
             return ' buttonchange'
         }
         return ''
@@ -17,20 +16,20 @@ class NavigationBar extends Component {
         return (
             <div>
                 <ul className='navigationbar-list '>
-                    <li className={'navigationbar-item '+ this.activeClassName('/Leave')}>
+                    <li className={'navigationbar-item ' + this.activeClassName('/Leave')}>
                         <Link to='/Leave'>leave</Link>
                     </li>
-                    <li className={'navigationbar-item'+ this.activeClassName('/History')}>
+                    <li className={'navigationbar-item' + this.activeClassName('/History')}>
                         <Link to='/History'>history</Link>
                     </li>
-                    <li className={'navigationbar-item'+ this.activeClassName('/SearchStatic')}>
+                    <li className={'navigationbar-item' + this.activeClassName('/SearchStatic')}>
                         <Link to='/SearchStatic'>Statistic</Link>
                     </li>
-                    <li className={'navigationbar-item'+ this.activeClassName('/Approve')}>
+                    <li className={'navigationbar-item' + this.activeClassName('/Approve')}>
                         <Link to='/Approve'>Approve</Link>
                     </li>
                     <li className='logoutbutton'>
-                        <Link to='/logout'><img src={logout}/>Logout</Link>
+                        <Link to='/logout'><img src={logout} />Logout</Link>
                     </li>
                 </ul>
 
